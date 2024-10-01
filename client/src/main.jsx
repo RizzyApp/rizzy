9360c2c3ccc6883b190a125ab2b25c67c6f29912
Kristóf Nyikes <kristof.nyikes@gmail.com>
2024-10-01 15:58:54 +0200

    feat: add react-router-dom to project

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
