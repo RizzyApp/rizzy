bcc160117ecf83f2565ebcbc7bb340b481812cee
hegedus-mark <hegedusmark2018@gmail.com>
2024-10-03 21:04:29 +0200

    chore: Create a button to reset the card for testing purposes #6

--- client/src/components/Swipe/SwipeCard.jsx
+++ client/src/components/Swipe/SwipeCard.jsx
@@ -2,55 +2,66 @@ import { useSpring, animated } from "@react-spring/web";
 import { useDrag } from "@use-gesture/react";
 import { forwardRef } from "react";
 
-
+const VELOCITY_THRESHOLD = 0.1;
+const MAX_ROTATION = 45;
 
 const SwipeCard = forwardRef(({ children, classname, deckWidth }, ref) => {
   const [{ x, rotateZ }, api] = useSpring(() => ({ x: 0, rotateZ: 0 }));
 
+  const reset = () => {
+    api.set({x: 0, rotateZ: 0})
+  }
 
-  const bind = useDrag(({active, movement: [mx], velocity: [vx], direction: [xDir]}) => {
-    const halfCardWidth = (ref.current?.offsetWidth || 0) / 2;
-    const boundary = deckWidth - halfCardWidth
-    const maxRotation = 45;
+  const bind = useDrag(
+    ({ active, movement: [mx], velocity: [vx], direction: [xDir] }) => {
+      const halfCardWidth = (ref.current?.offsetWidth || 0) / 2;
+      const boundary = deckWidth - halfCardWidth;
 
-    let rotation = (mx / boundary) * maxRotation;
+      let rotation = (mx / boundary) * MAX_ROTATION;
 
-    const clampedX = Math.min(Math.max(mx, -boundary), boundary);
-    const clampedRot = Math.min(Math.max(rotation, -maxRotation), maxRotation);
-    const trigger = vx > 0.1;
+      const clampedX = Math.min(Math.max(mx, -boundary), boundary);
+      const clampedRot = Math.min(
+        Math.max(rotation, -MAX_ROTATION),
+        MAX_ROTATION
+      );
+      const trigger = vx > VELOCITY_THRESHOLD;
 
-    if(!active && trigger){
-      api.start(i => {
-        console.log("start reached!");
-        const x = (200 + window.innerWidth) * xDir;
+      if (!active && trigger) {
+        api.start((i) => {
+          console.log("start reached!");
+          const x = (200 + window.innerWidth) * xDir;
 
-        return {
-          x,
-          rotateZ: 90,
-        }
-        })
-    } else{
-      api({ x: active ? clampedX : 0, immediate: active, rotateZ: active? clampedRot : 0 });
+          return {
+            x,
+            rotateZ: 90,
+          };
+        });
+      } else {
+        api({
+          x: active ? clampedX : 0,
+          immediate: active,
+          rotateZ: active ? clampedRot : 0,
+        });
+      }
     }
-
-
-    // Update the spring position
-
-  });
+  );
 
   return (
-    <animated.div
-      {...bind()}
-      className={classname}
-      ref={ref}
-      style={{
-        touchAction: "none",
-        x,
-        rotateZ,
-      }}
-    >
-      {children}
-    </animated.div>
+    <>
+      <animated.div
+        {...bind()}
+        className={classname}
+        ref={ref}
+        style={{
+          touchAction: "none",
+          x,
+          rotateZ,
+        }}
+      >
+        {children}
+      </animated.div>
+      <button onClick={reset}>Reset</button>
+    </>
   );
 });
 
