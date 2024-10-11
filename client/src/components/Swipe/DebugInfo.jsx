956090ad4e72a120dd9fd230e10f6f505e2cb0f0
hegedus-mark <hegedusmark2018@gmail.com>
2024-10-11 15:26:25 +0200

    fix: Resolve card flyout issue and add IS_DEVELOPMENT condition in useAnimation - Fixed the card flyout issue occuring when boundary was reached, but user was dragging the card back - Added IS_DEVELOPMENT condition to prevent unnecessary rerenders during debugging

--- client/src/components/Swipe/DebugInfo.jsx
+++ client/src/components/Swipe/DebugInfo.jsx
@@ -3,10 +3,14 @@ import React from "react";
 function DebugInfo({ debugInfo, reset }) {
   return (
     <div className="fixed top-0 left-0 p-4 bg-white text-black z-50">
-      <p>Movement X (mx): {debugInfo.mx.toFixed(2)}</p>
+      <p>Clamped Movment X (mx): {debugInfo.clampedX.toFixed(2)}</p>
+      <p>Cursor Movment X (mx): {debugInfo.currentMx.toFixed(2)}</p>
       <p>Rotation (deg): {debugInfo.rotation.toFixed(2)}</p>
       <p>Velocity: {debugInfo.velocity.toFixed(2)}</p>
-      <p>Direction: {debugInfo.direction === 1 ? "right" : "left"}</p>
+      <p>Direction: {debugInfo.direction}</p>
+      <p>
+        Direction Reversed: {debugInfo.isDirectionReversed ? "true" : "false"}
+      </p>
       <p>CardIndex: {debugInfo.index}</p>
       <p className={debugInfo.trigger ? "text-green-500" : "text-red-500"}>
         Trigger: {debugInfo.trigger ? "true" : "false"}
--- client/src/components/Swipe/hooks/useAnimation.js
+++ client/src/components/Swipe/hooks/useAnimation.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useSpring } from "@react-spring/web";
 import { useDrag } from "@use-gesture/react";
 
@@ -6,12 +6,38 @@ const VELOCITY_THRESHOLD = 0.1;
 const MAX_ROTATION = 20;
 const FLY_RANGE = 300;
 
-const calculateTrigger = (vx, mx, dir, boundary) => {
+const IS_DEVELOPMENT = import.meta.env.DEV;
+
+//Not sure which one would annoy the user most
+//Currently if it reaches the boundary and the cursor is moved back a little it will not trigger a fly out!
+
+const shouldTriggerFlyOut = (vx, mx, dir, boundary, reversedDirection) => {
+
+  if (reversedDirection) return false;
+
+  if (dir === 0 && Math.abs(mx) >= boundary) {
+    return true;
+  }
   if ((mx < 0 && dir !== -1) || (mx > 0 && dir !== 1)) {
     return false;
   }
   return vx > VELOCITY_THRESHOLD || Math.abs(mx) >= boundary;
 };
+
+const hasDirectionReversed = (currentMx, prevMx) => {
+  if (Math.abs(currentMx) < Math.abs(prevMx)) {
+    return true;
+  }
+  return false;
+}
+
+
+const hasKeptDirection = (currentMx, prevMx) => {
+  if (Math.abs(currentMx) > Math.abs(prevMx)) {
+    return true;
+  }
+  return false;
+}
 
 const calculateMovementAndBoundary = (mx, halfCardWidth, deckWidth) => {
   const boundary = deckWidth - halfCardWidth;
@@ -21,10 +47,13 @@ const calculateMovementAndBoundary = (mx, halfCardWidth, deckWidth) => {
 
 const useAnimation = (deckWidth, onSwipeOut) => {
   const [debugInfo, setDebugInfo] = useState({
-    mx: 0,
+    clampedX: 0,
+    currentMx: 0,
+    previousMx: 0,
     rotation: 0,
     velocity: 0,
     direction: 0,
+    isDirectionReversed: false,
     trigger: false,
   });
 
@@ -34,6 +63,21 @@ const useAnimation = (deckWidth, onSwipeOut) => {
     scale: 1,
     opacity: 1,
   }));
+
+  const prevMxRef = useRef(0);
+  const reversedDirectionRef = useRef(false);
+
+
+  const updateDirectionReversedRef = (currentMx) => {
+    const prevMx = prevMxRef.current;
+    const currentReversedDirectionState = hasDirectionReversed(currentMx, prevMx);
+
+    prevMxRef.current = currentMx;
+
+    if (hasDirectionReversed(currentMx, prevMx) || hasKeptDirection(currentMx, prevMx)) {
+      reversedDirectionRef.current = currentReversedDirectionState;
+    }
+  }
 
   const bind = useDrag(
     ({
@@ -50,15 +94,25 @@ const useAnimation = (deckWidth, onSwipeOut) => {
       }
 
       const { clampedX, boundary } = calculateMovementAndBoundary(mx, halfCardWidth, deckWidth);
-      const trigger = calculateTrigger(vx, mx, xDir, boundary);
 
-      setDebugInfo({
-        mx: clampedX,
-        rotation: (clampedX / boundary) * MAX_ROTATION,
-        velocity: vx,
-        direction: xDir,
-        trigger: trigger,
-      });
+
+      updateDirectionReversedRef(mx);
+
+      const trigger = shouldTriggerFlyOut(vx, mx, xDir, boundary, reversedDirectionRef.current);
+
+
+      if (IS_DEVELOPMENT) {
+        setDebugInfo({
+          clampedX: clampedX,
+          currentMx: mx,
+          previousMx: prevMxRef.current,
+          rotation: (clampedX / boundary) * MAX_ROTATION,
+          velocity: vx,
+          direction: xDir,
+          isDirectionReversed: reversedDirectionRef.current,
+          trigger: trigger,
+        });
+      }
 
       // Handle dragging animation
       if (active) {
@@ -72,7 +126,7 @@ const useAnimation = (deckWidth, onSwipeOut) => {
       } else if (!active && trigger) {
         // Trigger fallout animation
         api.start({
-          x: FLY_RANGE * xDir,
+          x: FLY_RANGE * (mx > 0 ? 1 : -1),
           rotateZ: (clampedX / boundary) * MAX_ROTATION,
           opacity: 0,
           onRest: onSwipeOut, // Callback when the fallout animation finishes
@@ -87,6 +141,8 @@ const useAnimation = (deckWidth, onSwipeOut) => {
   const reset = () => {
     // For debugging purposes
     api.set({ x: 0, rotateZ: 0, scale: 1, opacity: 1 });
+    prevMxRef.current = 0;
+    reversedDirectionRef.current = false;
   };
 
   // Combine all animated styles into one object
