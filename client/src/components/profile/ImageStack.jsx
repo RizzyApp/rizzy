fe7a1cd0879cf4ebd04bfb7261df1120a84b3bf1
hegedus-mark <hegedusmark2018@gmail.com>
2024-10-17 20:44:42 +0200

    feat: Create initial profilePage #32

---
+++ client/src/components/profile/ImageStack.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const db = [
+  {
+    id: 1,
+    name: "Mr.Bean",
+    src: "./image/bean-1.jpg",
+    bio: "This is a very cool bio",
+  },
+  {
+    id: 2,
+    name: "Mr.Bean",
+    src: "./image/bean-2.jpg",
+    bio: "This is a not so cool bio",
+  },
+  {
+    id: 3,
+    name: "Mr.Bean",
+    src: "./image/bean-3.jpg",
+    bio: "This is an okay bio",
+  },
+  {
+    id: 4,
+    name: "Mr.Bean",
+    src: "./image/bean-4.jpg",
+    bio: "This is a very bad bio",
+  },
+];
+
+function ImageStack() {
+  return <div>ImageStack</div>;
+}
+
+export default ImageStack;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -27,6 +27,12 @@ function HomePage() {
               >
                 Test SwipePage
               </button>
+              <button
+                onClick={() => navigate("/profile-page")}
+                className="absolute bottom-0 right-44 text-white"
+              >
+                Test ProfilePage
+              </button>
             </>
         </div>
       </div>
