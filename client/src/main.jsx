6f53636504ac47b87f82ee7f980012aa69473ada
Kristóf Nyikes <kristof.nyikes@gmail.com>
2024-10-29 15:54:50 +0100

    feat: add prompt screen to allow location

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,4 @@
-import { Children, StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
