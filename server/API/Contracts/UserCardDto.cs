26bfc57942cc5b2bc5ea8a63d929c0cee29325ad
Kristóf Nyikes <kristof.nyikes@gmail.com>
2024-11-12 16:48:37 +0100

    feat: add method to calculate distance based on coordinates

---
+++ server/API/Contracts/UserCardDto.cs
@@ -0,0 +1,5 @@
+﻿using API.Models;
+
+namespace API.Contracts;
+
+public record UserCardDto(int Id, string Name, int Age, string Bio, double Distance, IEnumerable<string> Photos);
--- server/API/Controllers/UserController.cs
+++ server/API/Controllers/UserController.cs
@@ -16,11 +16,14 @@ public class UserController : ControllerBase
 {
     private readonly IRepository<User> _repository;
     private readonly UserManager<IdentityUser> _userManager;
+    private readonly IUserService _userService;
 
-    public UserController(IRepository<User> repository, UserManager<IdentityUser> userManager) 
+
+    public UserController(IRepository<User> repository, UserManager<IdentityUser> userManager, IUserService userService) 
     {
         _repository = repository;
         _userManager = userManager;
+        _userService = userService;
     }
     [Authorize]
     [HttpPost]
@@ -45,5 +48,37 @@ public class UserController : ControllerBase
         await _repository.Add(result);
         
         return CreatedAtAction(nameof(PostUser), new CreateProfileResponse(result.Name, result.Gender, result.BirthDate, result.Bio));
+    }
+
+    [Authorize]
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<UserCardDto>>> GetSwipeUsers([FromQuery] decimal longitude,
+        [FromQuery] decimal latitude, [FromQuery] double preferredDistance, [FromQuery] int preferredMinAge, [FromQuery] int preferredMaxAge, [FromQuery] int preferredGender)
+    {
+        
+
+        return Ok();
+    }
+
+    private static double GetDistance(double latitude1, double longitude1, double latitude2, double longitude2)
+    {
+        const double earthRadiusKm = 6371.0;
+
+        var lat1 = latitude1 * Math.PI / 180.0;
+        var lon1 = longitude1 * Math.PI / 180.0;
+        var lat2 = latitude2 * Math.PI / 180.0;
+        var lon2 = longitude2 * Math.PI / 180.0;
+
+        var dlat = lat2 - lat1;
+        var dlon = lon2 - lon1;
+
+        var a = Math.Sin(dlat / 2) * Math.Sin(dlat / 2) +
+                   Math.Cos(lat1) * Math.Cos(lat2) *
+                   Math.Sin(dlon / 2) * Math.Sin(dlon / 2);
+
+        var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+        return earthRadiusKm * c;
+
     }
 }
