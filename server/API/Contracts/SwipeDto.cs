9500f0f05d3fc12ad0a57f9425f602d17a6dca7f
Kristóf Nyikes <kristof.nyikes@gmail.com>
2024-11-14 17:51:23 +0100

    feat: create Swipe controller

---
+++ server/API/Contracts/SwipeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Contracts;
+
+public class SwipeDto
+{
+    public int SwipedUserId { get; set; }
+    public string SwipeType { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
