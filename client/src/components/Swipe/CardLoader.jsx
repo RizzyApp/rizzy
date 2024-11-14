17e26052afb6796544003ed53b68a53b460a4bc0
Kristóf Nyikes <kristof.nyikes@gmail.com>
2024-11-14 15:33:14 +0100

    feat: slightly new design for swipe cards

--- client/src/components/Swipe/CardLoader.jsx
+++ client/src/components/Swipe/CardLoader.jsx
@@ -1,49 +1,57 @@
 import { useEffect, useState } from 'react';
-import SwipeDeck from "./SwipeDeck";
+import SwipeDeck from './SwipeDeck';
 
 const db = [
   {
     id: 1,
-    name: "Mr.Bean",
-    src: "./image/bean-1.jpg",
-    bio: "This is a very cool bio",
+    name: 'Mr.Bean',
+    photos: [],
+    bio: 'This is a very cool bio',
+    age: 30,
+    distance: 34,
   },
   {
     id: 2,
-    name: "Mr.Bean",
-    src: "./image/bean-2.jpg",
-    bio: "This is a not so cool bio",
+    name: 'Mr.Bean',
+    photos: ['./image/bean-1.jpg', './image/bean-2.jpg', './image/bean-3.jpg', './image/bean-3.jpg', './image/bean-4.jpg'],
+    bio: 'This is a not so cool bio',
+    age: 30,
+    distance: 34,
   },
   {
     id: 3,
-    name: "Mr.Bean",
-    src: "./image/bean-3.jpg",
-    bio: "This is an okay bio",
+    name: 'Mr.Bean',
+    photos: [],
+    bio: 'This is an okay bio',
+    age: 30,
+    distance: 34,
   },
   {
     id: 4,
-    name: "Mr.Bean",
-    src: "./image/bean-4.jpg",
-    bio: "This is a very bad bio",
+    name: 'Mr.Bean',
+    photos: ['./image/bean-1.jpg', './image/bean-2.jpg', './image/bean-3.jpg', './image/bean-3.jpg', './image/bean-4.jpg'],
+    bio: 'This is a very bad bio',
+    age: 30,
+    distance: 34,
   },
 ];
 
 function CardLoader() {
   const [users, setUsers] = useState(null);
-  
+
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch('/api/v1/User');
-      if(response.ok){
+      if (response.ok) {
         const data = await response.json();
         console.log(data);
         setUsers(data);
       }
-    }
+    };
     fetchData();
-  },[]);
+  }, []);
 
-  return <SwipeDeck initialCards={db} deckWidth={400}></SwipeDeck>;
+  return <SwipeDeck initialCards={users} deckWidth={400}></SwipeDeck>;
 }
 
 export default CardLoader;
